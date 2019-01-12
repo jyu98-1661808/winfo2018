2d324a7a680059081562f650538df4b001a2322c
jyu98-1661808 <jinayu@D-10-19-226-85.dhcp4.washington.edu>
2019-01-12 11:56:44 -0800

    Deleted files

--- splash/src/App.js
+++ splash/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 
-import './App.css';
+import './index.css';
 
 class App extends Component {
   render() {
@@ -51,6 +51,19 @@ class AboutUs extends Component {
     return (
       <section id="about-us">
 
+      </section>
+    );
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <section id="footer">
+        <p className="text-center my-4">
+          website created by jiyun yu using 
+          <img src="img/react-logo.png" alt="react logo" />
+        </p>
       </section>
     );
   }
