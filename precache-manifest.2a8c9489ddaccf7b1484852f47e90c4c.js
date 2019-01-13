80d9b72ca79c9402a9fec110c31c37688db42d06
jyu98-1661808 <jinayu@D-10-19-226-85.dhcp4.washington.edu>
2019-01-12 18:32:43 -0800

    Updates

---
+++ precache-manifest.2a8c9489ddaccf7b1484852f47e90c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43a7370a10319e36fa7165e62f799693",
+    "url": "/jyu98-1661808/winfo2018/static/media/background.43a7370a.jpeg"
+  },
+  {
+    "revision": "2038806787f043263dfe",
+    "url": "/jyu98-1661808/winfo2018/static/js/runtime~main.20388067.js"
+  },
+  {
+    "revision": "566997f73dcddc97901e",
+    "url": "/jyu98-1661808/winfo2018/static/js/main.566997f7.chunk.js"
+  },
+  {
+    "revision": "ef286e7cdb6abb610eb4",
+    "url": "/jyu98-1661808/winfo2018/static/js/1.ef286e7c.chunk.js"
+  },
+  {
+    "revision": "566997f73dcddc97901e",
+    "url": "/jyu98-1661808/winfo2018/static/css/main.de7ce5e2.chunk.css"
+  },
+  {
+    "revision": "733599cc0ecf683d3a512b871c7ac0e1",
+    "url": "/jyu98-1661808/winfo2018/index.html"
+  }
+];
